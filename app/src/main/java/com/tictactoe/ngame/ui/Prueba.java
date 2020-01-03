fc04f1040dc1567a6a7af0a7e1d821135bf34b29
Nnotro <nbravo1905@gmail.com>
2020-01-03 17:48:40 -0500

    solo prueba

---
+++ app/src/main/java/com/tictactoe/ngame/ui/Prueba.java
@@ -0,0 +1,16 @@
+package com.tictactoe.ngame.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.tictactoe.ngame.R;
+
+public class Prueba extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prueba);
+    }
+}
